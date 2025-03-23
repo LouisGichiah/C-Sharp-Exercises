d3401cebdee13b5d2c48dbe7b0bb8d540addf37a
LouisGichiah <louiegpaul@gmail.com>
2025-03-23 04:27:26 +0300

    simple counter - while loop

---
+++ Data types/counter.cs
@@ -0,0 +1,13 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        int counter = 0;
+        while(counter < 10)
+        {
+            Console.WriteLine(counter);
+            counter++;
+        }
+    }
+}
