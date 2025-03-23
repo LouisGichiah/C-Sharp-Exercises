e061573d496241608d6b075c9f1b43f09efe5e12
LouisGichiah <louiegpaul@gmail.com>
2025-03-23 04:45:37 +0300

    do while count

---
+++ Data types/do while counter.cs
@@ -0,0 +1,14 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        int counter=10;
+        do
+        {
+            Console.WriteLine(counter);
+            counter ++;
+        }
+        while(counter <10);
+    }
+}
