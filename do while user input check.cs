2f073f861df4f45a4d99b63d1555345fb90d9dd4
LouisGichiah <louiegpaul@gmail.com>
2025-03-24 04:40:10 +0300

    check input if between 1 and 10

---
+++ do while user input check.cs
@@ -0,0 +1,23 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        do
+        {
+            int userInput;
+            Console.WriteLine("Enter an integer between 1 and 10: ");
+            userInput = int.Parse(Console.ReadLine());
+                if (userInput >= 1 && userInput <= 10)
+                {
+                    Console.WriteLine($"The input {userInput} is valid");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"The input {userInput} is not valid");
+                }
+        }
+         while(true);
+    }
+}
