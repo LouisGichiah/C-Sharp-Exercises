9ec2ab96981171a9a9014ee6760585854131b08e
LouisGichiah <louiegpaul@gmail.com>
2025-03-27 02:52:04 +0300

    cacluclate area function

---
+++ function rectangle area.cs
@@ -0,0 +1,11 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        double function calculateArea (double length, double width)
+        {
+            return lenth * width;
+        }
+    }
+}
