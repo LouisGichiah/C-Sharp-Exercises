5bf3ee27f1e18abf630fe6d7172aa47027111503
LouisGichiah <louiegpaul@gmail.com>
2025-05-01 00:36:07 +0300

    Check driver age

---
+++ ChecksDriversAgeMethod.cs
@@ -0,0 +1,29 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the drivers age: ");
+        int driverAge = int.Parse(Console.ReadLine());
+        bool canDrive = IsOldEnoughToDrive(driverAge);
+        if(canDrive == true)
+        {
+            Console.WriteLine("The drivers age is valid");
+        }
+        else
+        {
+            Console.WriteLine("The drivers age is invalid!!");
+        }
+    }
+    public static bool IsOldEnoughToDrive(int age)
+    {
+        if(age>=18)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
