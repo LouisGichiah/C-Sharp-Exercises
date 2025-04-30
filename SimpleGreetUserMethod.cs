7ac0c4795ae1ba11f11ff227d0b9627146d36ead
LouisGichiah <louiegpaul@gmail.com>
2025-05-01 00:10:45 +0300

    greet user

---
+++ SimpleGreetUserMethod.cs
@@ -0,0 +1,12 @@
+using System;
+public class Program
+{
+    static void Main()
+    {
+        GreetUser("Louis");
+    }
+    public static void GreetUser(string name)
+    {
+        Console.WriteLine("Hello " +name+ "!");
+    }
+}
